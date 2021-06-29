1ff93e06bacd0ae66cf362d22cbafebfbf06e0e1
Batbold Gankhuyag <bgankhuyag@mail.roanoke.edu>
2021-06-29 22:24:50 +0800

    links

---
+++ src/demoData.js
@@ -0,0 +1,55 @@
+const data = [
+  {
+    name: "The Beautiful Country Side",
+    description: "This painting depicts the beauty of Mongolia's countryside",
+    date: "06-12-2021"
+  }, {
+    name: "The Beautiful Country Side",
+    description: "This painting depicts the beauty of Mongolia's countryside",
+    date: "06-12-2021"
+  }, {
+    name: "The Beautiful Country Side",
+    description: "This painting depicts the beauty of Mongolia's countryside",
+    date: "06-12-2021"
+  }, {
+    name: "The Beautiful Country Side",
+    description: "This painting depicts the beauty of Mongolia's countryside",
+    date: "06-12-2021"
+  }, {
+    name: "The Beautiful Country Side",
+    description: "This painting depicts the beauty of Mongolia's countryside",
+    date: "06-12-2021"
+  }, {
+    name: "The Beautiful Country Side",
+    description: "This painting depicts the beauty of Mongolia's countryside",
+    date: "06-12-2021"
+  }, {
+    name: "The Beautiful Country Side",
+    description: "This painting depicts the beauty of Mongolia's countryside",
+    date: "06-12-2021"
+  }, {
+    name: "The Beautiful Country Side",
+    description: "This painting depicts the beauty of Mongolia's countryside",
+    date: "06-12-2021"
+  }, {
+    name: "The Beautiful Country Side",
+    description: "This painting depicts the beauty of Mongolia's countryside",
+    date: "06-12-2021"
+  }, {
+    name: "The Beautiful Country Side",
+    description: "This painting depicts the beauty of Mongolia's countryside",
+    date: "06-12-2021"
+  }, {
+    name: "The Beautiful Country Side",
+    description: "This painting depicts the beauty of Mongolia's countryside",
+    date: "06-12-2021"
+  }, {
+    name: "The Beautiful Country Side",
+    description: "This painting depicts the beauty of Mongolia's countryside",
+    date: "06-12-2021"
+  }, {
+    name: "The Beautiful Country Side",
+    description: "This painting depicts the beauty of Mongolia's countryside",
+    date: "06-12-2021"
+  }, 
+]
---
+++ src/layout/header.js
@@ -0,0 +1,24 @@
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom'
+
+const Header = ({ children }) => {
+  return (
+    <>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link><Link to="/">Gallery</Link></Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link><Link to="about">About</Link></Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      {children}
+    </>
+  )
+}
+
+export default Header;
